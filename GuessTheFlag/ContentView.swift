4a334304b3b43b2bcc17e066bee4dcebf5608d6b
Ben Printac <bprintac1@gmail.com>
2022-10-11 02:37:47 +0300

    changed style and new score label

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,44 +11,50 @@ struct ContentView: View {
     
     @State private var ShowingScore = false
     @State private var scoreTitle = ""
+    @State private var totalPoints = 0
     
     @State private var countries = ["Estonia", "France", "Germany","Ireland","Italy","Nigeria","Poland","Russia", "Spain","UK","US"].shuffled()
-    @State private var correctAnswers = Int.random(in: 0...2)
+    @State private var correctAnswer = Int.random(in: 0...4)
     
     var body: some View {
         ZStack{
-            LinearGradient(colors: [.black,.green], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
+            LinearGradient(colors: [.green,.white], startPoint: .top, endPoint: .bottom).ignoresSafeArea()
                 VStack(spacing: 40){
                     VStack{
-                        Text("Tap the flag off").font(.subheadline.weight(.heavy))
-                        Text(countries[correctAnswers]).font(.largeTitle.weight(.semibold))
+                        Text("נחש את הדגל !").font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer]).font(.largeTitle.weight(.semibold))
                     }
-                    ForEach(0..<3){ num in
+                    ForEach(0..<5){ num in
                         Button{
                             flagTapped(num)
+                            
                         } label: {
                             Image(countries[num]).renderingMode(.original).clipShape(Capsule())
                                 .shadow(radius: 7)
                         }
                     }
+                    Text("נקודות: \(totalPoints)").foregroundColor(Color.green).bold()
+                
                     
                 }.alert(scoreTitle,isPresented: $ShowingScore){
-                    Button("Continue",action: askquestion)
-                }message: {
-                    Text("Your Answer is \(scoreTitle)")
+                    Button("המשך",action: askquestion)
                 }
             
             }
         }
         func flagTapped(_ number:Int){
-            if number == correctAnswers {
-                scoreTitle = "Correct"
-            } else { scoreTitle = "Wrong"}
+            if number == correctAnswer {
+                scoreTitle = "כל הכבוד!"
+                self.totalPoints += 1
+            } else {
+                scoreTitle = "אוי :("
+                self.totalPoints = 0
+            }
             ShowingScore = true
         }
         func askquestion(){
             countries.shuffle()
-            correctAnswers = Int.random(in: 0...2)
+            correctAnswer = Int.random(in: 0...4)
             
         }
     }
